c0bf66c5b267ddf7e5560eb214ffbb36439e419e
Albert O'Shea <albertoshea2@gmail.com>
2025-12-23 00:22:28 +1100

    stuff 2

--- nix/registry/mod/hm/shared/opencode/default.nix
+++ nix/registry/mod/hm/shared/opencode/default.nix
@@ -4,7 +4,7 @@
 */
 {
   __inputs = {
-    opencode-flake.url = "github:sst/opencode/v1.0.185";
+    opencode-flake.url = "github:sst/opencode/v1.0.191";
     opencode-flake.inputs.nixpkgs.follows = "nixpkgs";
   };
 
