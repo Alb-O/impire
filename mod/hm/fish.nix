539f20897d440801c5bade257855ce8065fcfc72
Albert O'Shea <albertoshea2@gmail.com>
2026-01-01 14:55:20 +1100

    fish: update prompt

--- mod/hm/fish.nix
+++ mod/hm/fish.nix
@@ -15,6 +15,53 @@ let
           y = "yazi";
           wt = "git worktree";
         };
+        functions.fish_prompt = ''
+          set -l last_status $status
+
+          # user@host
+          set_color brgreen
+          echo -n $USER@(prompt_hostname)
+          set_color normal
+
+          # git branch
+          set -l git_branch (fish_git_prompt '%s' 2>/dev/null)
+          if test -n "$git_branch"
+            set_color brmagenta
+            echo -n " 󰘬 $git_branch"
+            set_color normal
+          end
+
+          # nix shell indicator
+          if test -n "$IN_NIX_SHELL"
+            set_color brblue
+            echo -n '  '
+            set_color normal
+          end
+
+          # prompt suffix
+          if test $last_status -ne 0
+            set_color brred
+            echo -n '   '
+          else
+            set_color brblue
+            echo -n '   '
+          end
+          set_color normal
+        '';
+        functions.yy = ''
+          set tmp (mktemp -t "yazi-cwd.XXXXXX")
+          command yazi $argv --cwd-file="$tmp"
+          set cwd (cat "$tmp")
+          if test -n "$cwd" -a "$cwd" != "$PWD"
+            cd -- "$cwd"
+          end
+          rm -f -- "$tmp"
+        '';
+        functions.fish_right_prompt = ''
+          set_color brblack
+          echo -n ' '(prompt_pwd --full-length-dirs 100)
+          set_color normal
+        '';
         interactiveShellInit = ''
           abbr --add wt 'git worktree'
 
